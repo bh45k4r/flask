d404702a0c4d0c85e8a9fb2c649f38335a490e4b
Bhaskar Sengupta <dpaddr01-github@yahoo.com.au>
2015-12-28 18:17:44 -0800

    whatismyip in flask

---
+++ whats_my_ip/whats_my_ip.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def whats_my_ip():
+  """
+  returns remote address
+  """
+  return json.dumps({'remote_addr': request.remote_addr})
+
+
+if __name__ == '__main__':
+  app.run(
+           host = '::',
+           port = 80
+         )
