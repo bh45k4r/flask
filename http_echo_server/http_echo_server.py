23e0c8dd66332cf60b0ae2a251180d15aebaaee1
Bhaskar Sengupta <dpaddr01-github@yahoo.com.au>
2015-12-28 18:52:53 -0800

    http echo server in flask

---
+++ http_echo_server/http_echo_server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import json
+
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/echo', methods = ['GET', 'POST'])
+def echo():
+  """"
+  echo data
+  """
+  if request.method == 'GET':
+    return json.dumps(request.args)
+  else:
+    return json.dumps(request.form)
+
+
+if __name__ == '__main__':
+  app.run(host = '::',
+          port = 80)
